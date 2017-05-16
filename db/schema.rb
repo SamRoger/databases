400e960193ce8032e583e7fc9d1b46d0b7a1fa42
Sam Almeida <samRAlmeida@icloud.com>
2017-05-16 17:36:52 -0400

    create users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170516155240) do
+ActiveRecord::Schema.define(version: 20170516213233) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.string "category"
     t.text   "content"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password"
   end
 
 end
